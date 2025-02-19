ed4e23fc08e6be890ea61b2c693ac59dac223f66
SH-MU <SH-MU@users.noreply.github.com>
2025-02-19 14:42:38 +0200

    apply changes

--- script.js
+++
@@ -1,8 +0,0 @@
-function initializeWelcomeButton() {
-    var welcomeButton = document.getElementById("welcomeButton");
-    welcomeButton.addEventListener("click", function () {
-        // Your event handler code here
-    });
-}
-
-initializeWelcomeButton();
